c6c8f363b40ccc3c953fa0f6f8009010533f8cb7
鲁军 <1044635791@qq.com>
2018-11-15 01:33:25 +0800

    更新构建文件
    Signed-off-by: 鲁军 <1044635791@qq.com>

---
+++ YunPian.Test/SmsServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+using YunPian.Services;
+
+namespace YunPian.Test {
+    public class SmsServiceTest {
+        private readonly SmsService _smsService;
+        public SmsServiceTest()
+        {
+            var httpClientFactoryMock = Substitute.For<IHttpClientFactory>();
+            _smsService = new SmsService();
+        }
+        [Fact]
+        public void SingleSendAsyncTest () {
+
+        }
+    }
+}
